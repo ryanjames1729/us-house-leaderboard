70a6e575b96157153eebe8d825c6272576ee270e
Ryan James <ryanjames1729@gmail.com>
2024-01-09 11:11:14 -0500

    skeleton design done

--- app/page.tsx
+++ app/page.tsx
@@ -1,82 +1,84 @@
 import Image from 'next/image'
 
+'use client'
+
+import { Bar } from 'react-chartjs-2'
+import { Chart, CategoryScale, LinearScale, BarElement } from "chart.js"
+Chart.register(CategoryScale);
+Chart.register(LinearScale);
+Chart.register(BarElement);
+
 export default function Home() {
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="bg-secondary text-primary flex min-h-screen flex-col items-center justify-between p-24">
       
       <div className="relative flex place-items-center">
-        US Houses!
+      <h1 className={`mb-3 text-6xl font-semibold`}>US Houses!</h1>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-2 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="grid grid-col grid-cols-1 text-center">
+        <div className="shadow-2xl mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-64 lg:grid-cols-2 lg:text-left">
+          <div className="outline m-6 shadow-[0_0_20px_5px_#d41c46]">
+            <h2 className={`mb-3 text-4xl font-semibold`}>
+              Pine{' '}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            </div>
+            <div className="outline m-6 shadow-[0_0_20px_5px_#d41c46]">
+            <h2 className={`mb-3 text-4xl font-semibold`}>
+              Cardinal{' '}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            </div>
+            <div className="outline m-6 shadow-[0_0_20px_5px_#d41c46]">
+            <h2 className={`mb-3 text-4xl font-semibold`}>
+              Bell{' '}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            </div>
+            <div className="outline m-6 shadow-[0_0_20px_5px_#d41c46]">
+            <h2 className={`mb-3 text-4xl font-semibold`}>
+              Highlander{' '}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            </div>
+        </div>
       </div>
+
+
+      <div>
+          <Bar
+            data={{
+              labels: ["Pine", "Cardinal", "Bell", "Highlander"],
+              datasets: [
+                {
+                  label: "# of vulnerabilities",
+                  data: [150, 120, 60, 70],
+                  backgroundColor: ["green", "red", "blue", "black"],
+                  borderColor: "orange",
+                  borderWidth: 5
+                },
+              ]
+            }}
+            height={300}
+            width={500}
+            options={{
+              maintainAspectRatio: false
+            }}
+          />
+        </div>
+
+
+      <p className="mt-20 mb-0 font-white">This page was built by Ryan James.</p>
     </main>
   )
 }
