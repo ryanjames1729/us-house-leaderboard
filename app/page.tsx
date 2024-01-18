32d6063ecbe8f86326afa7f4bd33ede538e5c461
Ryan James <ryanjames1729@gmail.com>
2024-01-18 14:37:06 -0500

    made a few CSS changes. Server fetch not working...typescript kicking my butt

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,40 @@ import Image from 'next/image'
 
 'use client'
 
+import { GraphQLClient } from 'graphql-request'
+
 import { Bar } from 'react-chartjs-2'
 import { Chart, CategoryScale, LinearScale, BarElement } from "chart.js"
 Chart.register(CategoryScale);
 Chart.register(LinearScale);
 Chart.register(BarElement);
 
-export default function Home() {
+async function getHouses() {
+  const GRAPHCMS_URL_ENDPOINT = 'https://api-us-east-1-shared-usea1-02.hygraph.com/v2/clrjkusf007bv01vx43bnq4kz/master'
+  const graphcms = new GraphQLClient(GRAPHCMS_URL_ENDPOINT)
+
+  const { houses }: { houses: Array<{ houseName: string, housePoints: number }> } = await graphcms.request(
+    `
+    {
+      houses {
+        houseName
+        housePoints
+      }
+    }
+    `
+  )
+
+  return houses
+}
+
+
+export default function Home({ houses }: { houses: Array<{ houseName: string, housePoints: number }> }) {
+  // const houses: Array<string> = getHouses();
+
+
+  const houseData = houses;
+  console.log("house data")
+  console.log(houseData);
   
   return (
     <main className="bg-secondary text-primary flex min-h-screen flex-col items-center justify-between p-24">
@@ -19,66 +46,76 @@ export default function Home() {
 
       <div className="grid grid-col grid-cols-1 text-center">
         <div className="shadow-2xl mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-64 lg:grid-cols-2 lg:text-left">
-          <div className="outline m-6 shadow-[0_0_20px_5px_#d41c46]">
-            <h2 className={`mb-3 text-4xl font-semibold`}>
+
+          <a href="#houses">
+          <div className="outline m-6 shadow-[0_0_20px_5px_#d41c46] hover:shadow-[0_0_30px_10px_#d41c46] h-24 grid-cols-1 text-center">
+            <h2 className="mb-3 text-4xl font-semibold py-6">
               Pine{' '}
               <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                 -&gt;
               </span>
             </h2>
-            </div>
-            <div className="outline m-6 shadow-[0_0_20px_5px_#d41c46]">
-            <h2 className={`mb-3 text-4xl font-semibold`}>
+            </div></a>
+
+            <a href="#houses">
+            <div className="outline m-6 shadow-[0_0_20px_5px_#d41c46] hover:shadow-[0_0_30px_10px_#d41c46] h-24 grid-cols-1 text-center">
+            <h2 className="mb-3 text-4xl font-semibold py-6">
               Cardinal{' '}
               <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                 -&gt;
               </span>
             </h2>
-            </div>
-            <div className="outline m-6 shadow-[0_0_20px_5px_#d41c46]">
-            <h2 className={`mb-3 text-4xl font-semibold`}>
+            </div></a>
+
+            <a href="#houses">
+            <div className="outline m-6 shadow-[0_0_20px_5px_#d41c46] hover:shadow-[0_0_30px_10px_#d41c46] h-24 grid-cols-1 text-center">
+            <h2 className="mb-3 text-4xl font-semibold py-6">
               Bell{' '}
               <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                 -&gt;
               </span>
             </h2>
-            </div>
-            <div className="outline m-6 shadow-[0_0_20px_5px_#d41c46]">
-            <h2 className={`mb-3 text-4xl font-semibold`}>
+            </div></a>
+
+            <a href="#houses">
+            <div className="outline m-6 shadow-[0_0_20px_5px_#d41c46] hover:shadow-[0_0_30px_10px_#d41c46] h-24 grid-cols-1 text-center">
+            <h2 className="mb-3 text-4xl font-semibold py-6">
               Highlander{' '}
               <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                 -&gt;
               </span>
             </h2>
-            </div>
+            </div></a>
         </div>
       </div>
 
 
-      <div>
+      <div id="houses">
           <Bar
             data={{
               labels: ["Pine", "Cardinal", "Bell", "Highlander"],
               datasets: [
                 {
-                  label: "# of vulnerabilities",
+                  label: "US House Points",
                   data: [150, 120, 60, 70],
-                  backgroundColor: ["green", "red", "blue", "black"],
-                  borderColor: "orange",
-                  borderWidth: 5
+                  backgroundColor: ["rgba(0,255,0,0.3)", "rgba(255,0,0,0.3)", "rgba(0,0,255,0.3)", "rgba(0,0,0,0.3)"],
+                  borderColor: "red",
+                  borderWidth: 2,
                 },
               ]
             }}
             height={300}
             width={500}
             options={{
-              maintainAspectRatio: false
+              maintainAspectRatio: false,
+              responsive: true,
             }}
+          
           />
         </div>
 
 
-      <p className="mt-20 mb-0 font-white">This page was built by Ryan James.</p>
+      <p className="mt-20 pt-10 mb-0 font-white">This page was built by Ryan James.</p>
     </main>
   )
 }
